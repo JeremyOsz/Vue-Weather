ae00c16859f9d3991f51f94d6e6362e66b050ab9
JeremyOsz <j.osztreicher@gmail.com>
2018-11-25 19:48:24 +1100

    Initial commit, today added

---
+++ src/components/Today.vue
@@ -0,0 +1,20 @@
+<template id="Today">
+  <p>{{weather}}</p>
+</template>
+
+<script type="text/javascript">
+  module.exports = {
+    data: function(){
+      return{
+        weather: '24 degrees'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    color: #999999;
+  }
+</style>
